d8e67195d03da284d0c57efee3fa7fe00d4e8eed
knyttali <amanda_andrae@hotmail.com>
2022-01-10 14:32:33 +0100

    hej

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        hej
         Random random = new Random();
         int maxNrOfCones = 100;
         int maxNrOfSquirrels = 20;
