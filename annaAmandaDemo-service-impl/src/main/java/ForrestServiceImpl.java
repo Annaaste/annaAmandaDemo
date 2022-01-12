4de33576d69de60d3e458b72bf5fb4081a146b4b
knyttali <amanda_andrae@hotmail.com>
2022-01-12 16:37:24 +0100

    funkar med

---
+++ annaAmandaDemo-service-impl/src/main/java/ForrestServiceImpl.java
@@ -0,0 +1,9 @@
+import annaAmandaDemo.domain.Owl;
+
+public class ForrestServiceImpl {
+    Owl owl;
+}
+/*
+* b. Den ska ha metoderna createOwl, createPineTree och createSquirrel. De ska
+anropa klassernas konstruktorer med new och returnera en instans av klassen.
+* */
